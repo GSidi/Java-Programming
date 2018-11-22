90db753a8bfca3574899bb6b10fc5e3e45c87157
GSidi <45037105+GSidi@users.noreply.github.com>
2018-11-23 00:08:41 +0200

    Add files via upload

---
+++ JavaEx1.java
@@ -0,0 +1,14 @@
+
+package javaex1;
+
+public class JavaEx1 {
+
+   
+    public static void main(String[] args) {  
+       /* System.out.print("welcome to ");
+        System.out.print("Java Programming"); */
+       
+       System.out.printf("%s%n%s%n","Welcome to","Java Programming");
+    }
+    
+}
